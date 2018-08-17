3ebcdf2083894164e2aca014ac7cd5266b828cdb
Anatoly Makarevich <anatoly_mak@yahoo.com>
2018-08-17 10:03:06 +0300

    Base objects.

---
+++ mafia/core/__init__.py
@@ -0,0 +1,17 @@
+
+class StateObject(object):
+    """Base class for stateful objects."""
+
+    pass
+
+
+class DeltaObject(object):
+    """Base class for objects that change states."""
+
+    pass
+
+
+class Engine(StateObject):
+    """"""
+
+    pass
