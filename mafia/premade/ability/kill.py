4aa1761e8150e7ba8e77972cbbf7ec71f04d2861
Anatoly Makarevich <anatoly_mak@yahoo.com>
2019-03-13 22:50:14 +0300

    Aded placeholder for killing stuff.

---
+++ mafia/premade/ability/kill.py
@@ -0,0 +1,32 @@
+
+# from mafia.core.event import InternalEvent, Subscriber, EventManager
+from mafia.core.action import Action  # , PreActionEvent, PostActionEvent
+from mafia.state.role import ActivatedAbility
+
+
+class KillAction(Action):
+    """Action that causes death.
+    
+    Attributes
+    ----------
+    source : GameObject
+        Who/what is killing; usually an :class:`Actor`.
+    target : GameObject
+        Who/what is being killed; usually an :class:`Actor`). 
+    """
+
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+
+    def _execute(self):
+        pass  # TODO: Need to cause death 
+
+
+class KillAbility(ActivatedAbility):
+    """Allows an actor to kill."""
+
+    def __init__(self):
+        super().__init__() 
+
+    # TODO: Add "do the thing" here. 
