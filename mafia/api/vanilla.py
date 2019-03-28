02b6eb9b4090a3a3340f3085fdf6b61b6b9c9ffe
Anatoly Makarevich <anatoly_mak@yahoo.com>
2019-03-28 23:22:47 +0300

    Added very simple lobby manipulation. Added game start/stop.

--- mafia/api/vanilla.py
+++ mafia/api/vanilla.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Callable
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
 from mafia.premade.template.vanilla import VanillaGame 
 from mafia.core.event import EventManager
@@ -8,18 +9,27 @@ from mafia.state.actor import ActorControlEvent
 import random
 
 
+class Lobby(BaseModel):
+    """"""
+
+    players: List[str] = []
+
+
 class MafiaAPI(APIRouter):
 
     @classmethod
     def generate(cls, players=5):
         vg = VanillaGame.generate(players)
-        ex = cls(vg)
+        player_names = [a.name.lower() for a in vg.actors]
+        lb = Lobby(players=player_names)
+        ex = cls(lb, vg)
         return ex
     
-    def __init__(self, game):
+    def __init__(self, lobby: Lobby = Lobby(), game=None):
         super().__init__()
 
-        self.game = game
+        self.lobby = lobby
+        self._game = game
 
         # Status
         self.get("/")(self.root)
@@ -29,28 +39,81 @@ class MafiaAPI(APIRouter):
         self.get("/players/{player_name}", response_model=dict)(
             self.get_player_info)
         self.get("/game", response_model=dict)(self.get_game_status)
+        self.get("/game/winner", response_model=str)(self.get_winning_team)
+        self.get("/lobby", response_model=List[str])(self.get_lobby_players)
 
         # Action
         self.post("/next-phase")(self.apply_next_phase)
         self.post("/ability/{source}")(self.apply_ability)
         self.post("/random/vote")(self.apply_vote_random)
         self.post("/random/mkill")(self.apply_mkill_random)
+        self.post("/lobby/add")(self.add_player_to_lobby)
+        self.post("/lobby/remove")(self.remove_player_from_lobby)
+        self.post("/game/start")(self.start_game)
+        self.post("/game/end", response_model=str)(self.end_game)
         
+    @property
+    def started(self):
+        return (self._game is not None)
+
+    @property
+    def game(self):
+        if not self.started:
+            raise HTTPException(400, "Game not yet started.")
+        return self._game
+    
+    @game.setter
+    def game(self, value):
+        self._game = value
+
     def root(self):
         """Gets the API version."""
         return {'api-version': 0.1}
 
+    def get_lobby_players(self):
+        """Lists players in the lobby."""
+        return self.lobby.players
+
+    def add_player_to_lobby(self, player_name: str):
+        """Adds the player to the lobby."""
+        self.lobby.players.append(player_name)
+
+    def remove_player_from_lobby(self, player_name: str):
+        """Removes the player from the lobby."""
+        try:
+            self.lobby.players.remove(player_name)
+        except ValueError:
+            raise HTTPException(400, "No such player in lobby.")
+
+    def start_game(self):
+        if self.started:
+            raise HTTPException(400, "Game already in progress.")
+        self.game = VanillaGame.generate(players=self.lobby.players)
+
+    def end_game(self):
+        if not self.started:
+            raise HTTPException(400, "Game not started yet.")
+        winner = self.get_winning_team()
+        self.game = None
+        return winner
+
     def get_game_status(self):
         """Returns dict of phase, alignments, votes."""
-        g = self.game 
-        res = {
-            'phase': g.phase_state.states[g.phase_state.current], 
-            'alignments': [a.name for a in g.alignments],
-            'vote_tally': {
-                k.name: v.name
-                for k, v in g.vote_tally.votes_for.items()
-            }, 
-        }
+        if self.started:
+            g = self.game 
+            res = {
+                'started': True, 
+                'phase': g.phase_state.states[g.phase_state.current], 
+                'alignments': [a.name for a in g.alignments],
+                'vote_tally': {
+                    k.name: v.name
+                    for k, v in g.vote_tally.votes_for.items()
+                }, 
+            }
+        else:
+            res = {
+                'started': False, 
+            }
         return res
 
     def list_players(
@@ -67,17 +130,10 @@ class MafiaAPI(APIRouter):
                 Q = Q & (a.status['alive'] == alive)
             return Q
                 
-        z = self._list_players(team=team_name, filt=filt)
+        z = self._list_players(team_name=team_name, filt=filt)
         return [a.name for a in z]     
-
-    def list_alive(self, team_name: str = None):
-        """Returns list of players (names) that are alive."""
-        return self.list_players(
-            team=team_name, 
-            filt=lambda ac: ac.status['alive']
-        )
-
-    def _list_players(self, team=None, filt=None):
+    
+    def _list_players(self, team_name: str = None, filt: Callable = None):
         """Returns (filtered) list of players. 
 
         Internal function (not exposed as API).
@@ -89,12 +145,12 @@ class MafiaAPI(APIRouter):
         filt : None or callable
             Boolean function with argument :class:`Actor`.
         """
-        if team is None:
+        if team_name is None:
             pool = self.game.actors
         else:
             pool = [
                 al for al in self.game.alignments
-                if al.name == team
+                if al.name.lower() == team_name.lower()
             ][0].members
 
         if filt is None:
@@ -139,10 +195,10 @@ class MafiaAPI(APIRouter):
     def get_winning_team(self):
         """Returns name of winning team, or None if no winner."""
 
-        if len(self.list_alive(team='town')) == 0:
+        if len(self.list_players(team_name='town', alive=True)) == 0:
             return 'mafia'
         
-        if len(self.list_alive(team='mafia')) == 0:
+        if len(self.list_players(team_name='mafia', alive=True)) == 0:
             return 'town'
         
         return None
@@ -175,13 +231,13 @@ class MafiaAPI(APIRouter):
         """Adds a random vote."""
 
         ability_name = 'lynch-vote'
-        source, target = random.choices(self.list_alive(), k=2)
+        source, target = random.choices(self.list_players(alive=True), k=2)
         self.apply_ability(source, ability_name, target)
 
     def apply_mkill_random(self):
         """Adds a random Mafia kill."""
 
         ability_name = 'mafia-kill'
-        source = random.choice(self.list_alive(team='mafia'))
-        target = random.choice(self.list_alive(team='town'))
+        source = random.choice(self.list_players(team_name='mafia', alive=True))
+        target = random.choice(self.list_players(team_name='town', alive=True))
         self.apply_ability(source, ability_name, target)
