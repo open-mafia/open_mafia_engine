866ba6adeabe4d86159a388bef73006cf5b66640
Anatoly Makarevich <anatoly_mak@yahoo.com>
2019-03-28 23:31:18 +0300

    Changed to not auto-generate game and lobby.

--- mafia/api/main.py
+++ mafia/api/main.py
@@ -4,5 +4,6 @@ from mafia.api.vanilla import MafiaAPI
 
 #
 app = FastAPI()
-mafia_api = MafiaAPI.generate(5)
+# mafia_api = MafiaAPI.generate(5)
+mafia_api = MafiaAPI()
 app.include_router(mafia_api)
