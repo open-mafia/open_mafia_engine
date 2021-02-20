04ad64c517773774055ded2b6b8226295752b587
Anatoly Makarevich <anatoly_mak@yahoo.com>
2021-02-20 15:37:02 +0300

    Added "constraint" with built-ins, subclass hooks

---
+++ open_mafia_engine/built_in/load.py
@@ -0,0 +1,8 @@
+"""Loads all built-ins."""
+
+from pathlib import Path
+
+from open_mafia_engine.core.constraint import Constraint
+from open_mafia_engine.util.yaml import load_yaml
+
+Constraint.update_builtins(load_yaml(Path(__file__).parent / "constraint.yml"))
