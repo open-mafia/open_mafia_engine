8a6fed2b2ce8c4942d3ea813cd95a48a504dae0f
Anatoly Makarevich <anatoly_mak@yahoo.com>
2021-02-21 01:34:11 +0300

    Added basic wincons.

--- open_mafia_engine/built_in/load.py
+++ open_mafia_engine/built_in/load.py
@@ -5,8 +5,10 @@ from pathlib import Path
 from open_mafia_engine.core.ability import Ability
 from open_mafia_engine.core.constraint import Constraint
 from open_mafia_engine.core.phase import Phase
+from open_mafia_engine.core.wincon import Wincon
 from open_mafia_engine.util.yaml import load_yaml
 
 Ability.update_builtins(load_yaml(Path(__file__).parent / "ability.yml"))
 Constraint.update_builtins(load_yaml(Path(__file__).parent / "constraint.yml"))
 Phase.update_builtins(load_yaml(Path(__file__).parent / "phase.yml"))
+Wincon.update_builtins(load_yaml(Path(__file__).parent / "wincon.yml"))
