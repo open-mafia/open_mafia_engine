b4b91cf12ff9b7d88a2c2b60386c2ec0c043622b
Anatoly Makarevich <anatoly_mak@yahoo.com>
2021-03-07 18:43:44 +0300

    Added actor status.

--- open_mafia_engine/state/actor.py
+++ open_mafia_engine/state/actor.py
@@ -1,5 +1,8 @@
-from .base import StateModel
+from typing import Any, Dict
+
 from open_mafia_engine.state.role import Role
+
+from .base import StateModel
 
 
 class Actor(StateModel):
@@ -11,7 +14,10 @@ class Actor(StateModel):
         The name is used as an identifier.
     role : Role
         The role
+    status : Dict[str, Any]
+        Current game status.
     """
 
     name: str
     role: Role
+    status: Dict[str, Any] = {}
