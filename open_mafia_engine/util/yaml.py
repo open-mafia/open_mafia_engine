2ef67eb37ba3d34872d0b64a4e49a91d88310a21
Anatoly Makarevich <git@nowan.dev>
2021-05-16 13:27:32 +0200

    Option to use either pyyaml or ruamel.yaml

--- open_mafia_engine/util/yaml.py
+++ open_mafia_engine/util/yaml.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 from typing import Union
 
-import yaml
+try:
+    import yaml
 
-# TODO: Support ruamel.yaml as well
+    def load_yaml(p: Path) -> Union[dict, list, None]:
+        """Loads a YAML file safely."""
+
+        p = Path(p).resolve()
+        with p.open() as f:
+            return yaml.safe_load(f)
 
 
-def load_yaml(p: Path) -> Union[dict, list, None]:
-    """Loads a YAML file safely."""
+except ImportError:
+    from ruamel.yaml import YAML
 
-    p = Path(p).resolve()
-    with p.open() as f:
-        return yaml.safe_load(f)
+    def load_yaml(p: Path) -> Union[dict, list, None]:
+        p = Path(p).resolve()
+        with p.open() as f:
+            return YAML(typ="safe").load(f)
