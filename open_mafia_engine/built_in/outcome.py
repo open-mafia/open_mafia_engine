34142c83201190b27a3c191a03cf542104839255
Anatoly Makarevich <git@nowan.dev>
2021-05-16 20:22:08 +0200

    Fixed outcome checking to do one or more factions.

--- open_mafia_engine/built_in/outcome.py
+++ open_mafia_engine/built_in/outcome.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional, Union
 
 from open_mafia_engine.core import (
     Actor,
@@ -16,15 +16,20 @@ class FactionEliminatedOutcome(OutcomeChecker):
     """Win (or lose) when the target faction has been eliminated."""
 
     def __init__(
-        self, parent: Alignment, faction_name: str, outcome: Outcome = Outcome.victory
+        self,
+        parent: Alignment,
+        faction_names: Union[str, List[str]],
+        outcome: Outcome = Outcome.victory,
     ):
         super().__init__(parent)
-        self._faction_name = faction_name
+        if isinstance(faction_names, str):
+            faction_names = [faction_names]
+        self._faction_names = faction_names
         self._outcome = Outcome(outcome)
 
     @property
-    def faction_name(self) -> str:
-        return self._faction_name
+    def faction_names(self) -> str:
+        return self._faction_names
 
     @property
     def outcome(self) -> Outcome:
@@ -39,10 +44,11 @@ class FactionEliminatedOutcome(OutcomeChecker):
     def respond_to_event(self, event: Event, game: Game) -> Optional[OutcomeAction]:
         if isinstance(event, EStatusChange):
             if event.key == "dead":
-                # Someone died, maybe we win?
-                al: Alignment = game.alignments[self.faction_name]
-                if all(actor.status["dead"] for actor in al.actors):
-                    return OutcomeAction(
-                        self, alignment=self.parent, outcome=self.outcome
-                    )
+                # Someone died, maybe we win (lose)?
+                for fn in self.faction_names:
+                    al: Alignment = game.alignments[fn]
+                    if not all(actor.status["dead"] for actor in al.actors):
+                        return None
+                # Hey, all of them died - we win (lose)!
+                return OutcomeAction(self, alignment=self.parent, outcome=self.outcome)
         return None
