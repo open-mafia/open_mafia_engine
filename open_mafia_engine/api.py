88e9be56ddadc24a8a0c5978b1d036f0b734adad
Anatoly Makarevich <git@nowan.dev>
2021-05-16 22:17:56 +0200

    Minor changes.

---
+++ open_mafia_engine/api.py
@@ -0,0 +1,7 @@
+"""This should be all you need to start.
+
+TODO: Figure out the minimum imports!
+"""
+
+from open_mafia_engine.core import Game
+from open_mafia_engine.prefab import Prefab  # this will load all built-ins
