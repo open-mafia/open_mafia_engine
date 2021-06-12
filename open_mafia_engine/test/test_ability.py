cf1c9d3ef1f09a7840dfd55e77290a73d3ce2c11
Anatoly Makarevich <git@nowan.dev>
2021-06-12 16:06:37 +0200

    Added test for action and ability generation.

--- open_mafia_engine/test/test_ability.py
+++ open_mafia_engine/test/test_ability.py
@@ -1,6 +1,8 @@
 from open_mafia_engine.built_in.voting import Tally
-from open_mafia_engine.core.state import EActivate
+from open_mafia_engine.core.event_system import Action
+from open_mafia_engine.core.state import Ability, EActivate
 from open_mafia_engine.builders.for_testing import make_test_game
+
 
 def test_activation_by_event():
     """Tests activation via events, directly or via converters."""
@@ -19,3 +21,28 @@ def test_activation_by_event():
         EActivate(game, "Alice/ability/Vote", target="unvote"), process_now=True
     )
     assert tally.results.vote_leaders == []
+
+
+def test_gen_action_ability():
+    """Tests action and ability generation."""
+
+    actor_names = ["Alpha", "Bravo"]
+
+    def fake_action(self: Action):
+        assert self.game.actor_names == actor_names
+
+    AcFake = Action.generate(fake_action, name="AcFake", doc="Fake action")
+    AbFake = Ability.generate(AcFake, name="AbFake", doc="Fake ability")
+
+    @Ability.generate
+    def AbFake2(self: Action):
+        """fake ability 2"""
+        assert self.game.actor_names == actor_names
+
+    game = make_test_game(actor_names)
+
+    a_abil = AbFake(game, owner=game.actors[0], name="a_abil")
+    a_abil.activate()
+
+    b_abil = AbFake2(game, owner="Bravo", name="b_abil")
+    b_abil.activate()
