f779b5c9db912af2d500cdbfedf1effa8fc3fb3f
Anatoly Makarevich <git@nowan.dev>
2021-06-17 22:38:30 +0200

    Made ".api" to allow simpler imports.

---
+++ open_mafia_engine/api/__init__.py
@@ -0,0 +1,10 @@
+"""API package.
+
+Use as the following:
+
+    import open_mafia_engine.api as mafia
+    mafia.Game
+"""
+
+from open_mafia_engine.core.all import *
+from open_mafia_engine.built_in.all import *
