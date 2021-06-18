517bc10d9475c31e70c2fca324fba5b0d612a561
Anatoly Makarevich <git@nowan.dev>
2021-06-18 21:20:19 +0200

    Implemented a lot of CommandRunner commands. :)

--- open_mafia_engine/commands/runner.py
+++ open_mafia_engine/commands/runner.py
@@ -1,25 +1,15 @@
 from __future__ import annotations
 
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    Generic,
-    List,
-    Optional,
-    Tuple,
-    Type,
-    Union,
-)
+from typing import Any, Callable, Dict, Generic, List, Optional, Tuple, Type, Union
 
 from makefun import partial
 
-from open_mafia_engine.core.all import Game
+from open_mafia_engine.core.all import ABILITY, EActivate, Game, GameBuilder, get_path
 from open_mafia_engine.util.matcher import FuzzyMatcher
 
 from .lobby import AbstractLobby, SimpleLobby
-from .parser import AbstractCommandParser
-from .raw import TUser, RawCommand
+from .parser import AbstractCommandParser, ShellCommandParser
+from .raw import RawCommand, TUser
 
 __all__ = ["command", "CommandHandler", "CommandRunner"]
 
@@ -122,32 +112,65 @@ def command(
 
 
 class CommandRunner(Generic[TUser]):
-    """Runs the most important commands.
+    """Interprets and runs commands on the Mafia engine.
 
-    TODO: Docstring.
+    This handles both pre-game commands (such as joining or starting the game)
+    and commands during the game.
+    Basic Mafia commands are already added.
+
+    To add or override a command handler, use the `command` decorator:
+
+        class MyRunner(CommandRunner[MyUserType]):
+            @command("blah", game=True)
+            def my_blah(self, source: str, *args, **kwargs):
+                return
+
+    Attributes
+    ----------
+    parser : AbstractCommandParser
+        Your own parser implementation, or ShellCommandParser() by default.
+    lobby : AbstractLobby
+        Your own lobby implementation, or SimpleLobby[TUser]() by default.
+    game : None or Game
+        The game state. Defaults to None.
+    score_cutoff : int
+
     """
 
     registered_commands: Dict[str, CommandHandler]
 
     def __init__(
         self,
-        parser: AbstractCommandParser,
-        lobby: AbstractLobby[TUser] = SimpleLobby(),
+        parser: AbstractCommandParser = ShellCommandParser(),
+        lobby: AbstractLobby[TUser] = SimpleLobby[TUser](),
         game: Optional[Game] = None,
         *,
         score_cutoff: int = 80,
     ):
         self.parser = parser
         self.lobby = lobby
-        self.game = game
         self.score_cutoff = int(score_cutoff)
+        # This will check :)
+        self.game = game
+
+    @property
+    def game(self) -> Optional[Game]:
+        return self._game
+
+    @game.setter
+    def game(self, game: Optional[Game]):
+        if game is not None and not isinstance(game, Game):
+            raise TypeError(f"Expected None or Game, got {game!r}")
+        self._game = game
 
     @property
     def in_game(self) -> bool:
-        return self.game is not None
+        """True if the game is active."""
+        return self._game is not None
 
     @property
     def in_lobby(self) -> bool:
+        """True if the game is not active."""
         return not self.in_game
 
     def parse_and_run(self, source: str, obj: str) -> List[Tuple[RawCommand, Any]]:
@@ -168,7 +191,10 @@ class CommandRunner(Generic[TUser]):
         return matcher[name]
 
     def dispatch(self, rc: RawCommand) -> Any:
-        """Calls the relevant command given by `rc`."""
+        """Calls the relevant command given by `rc`.
+
+        TODO: Backup handler for in-game and in-lobby handlers.
+        """
         ch: CommandHandler = self.find_command(rc.name)
         if ch.admin:
             if rc.source not in self.lobby.admin_names:
@@ -177,6 +203,8 @@ class CommandRunner(Generic[TUser]):
             raise RuntimeError(f"Command {ch.name!r} not available in lobby.")
         if self.in_game and not ch.game:
             raise RuntimeError(f"Command {ch.name!r} not available during game.")
+
+        # TODO: Possibly add debug output here?
         return ch.func(self, rc.source, *rc.args, **rc.kwargs)
 
     def user_for(self, name: str) -> Optional[TUser]:
@@ -191,45 +219,75 @@ class CommandRunner(Generic[TUser]):
             raise ValueError(f"No user found for {source!r}. Possibly not implemented?")
         self.lobby.add_player(source, user)  # [source] = user
 
-    @command("force-join", admin=True, lobby=True)
-    def force_join(self, source: str, *args, **kwargs):
-        """Force a user to join the lobby.
+    @command("in", lobby=True)
+    def _join2(self, source: str):
+        """Alias for 'join'."""
+        return self.join(source)
 
-        TODO: Implement!
-        """
+    @command("force-join", admin=True, lobby=True)
+    def force_join(self, source: str, *targets: List[str]):
+        """Force one or more users to join the lobby."""
+        users: Dict[str, TUser] = {}
+        errors: List[str] = []
+        for t in targets:
+            user = self.user_for(t)
+            if user is None:
+                errors.append(t)
+            else:
+                users[t] = user
+        if len(errors) > 0:
+            raise ValueError(f"No users found for these names: {errors!r}.")
+        for n, u in users.items():
+            self.lobby.add_player(n, u)
 
     @command("kick", admin=True, lobby=True, game=True)
     def kick(self, source: str, *args, **kwargs):
         """Kick a player from the lobby or game.
 
         TODO: Implement!
+        Will probably need change in AbstractLobby.
+        Will definitely need change in Game to handle kicking/modkilling...
         """
 
-    @command("in", lobby=True)
-    def join2(self, source: str):
-        """Alias for 'join'."""
-        return self.join(source)
-
     @command("create-game", admin=True, lobby=True)
-    def create_game(self, source: str, *args, **kwargs):
+    def create_game(self, source: str, builder_name: str, *args, **kwargs):
         """Creates the game.
 
         TODO: Implement!
         """
+        if self.in_game:
+            raise ValueError("BUG. Currently in a game - can't create one.")
+        builder = GameBuilder.load(builder_name)
+        game = builder.build(player_names=self.lobby.player_names, *args, **kwargs)
+        self.game = game
+
+    @command("destroy-game", admin=True, game=True)
+    def destroy_game(self, source: str):
+        """Stops the game immediately.
+
+        Do we even need this? I assume you'd just make a new runner... :)
+
+        TODO: Maybe a bit more elegantly?...
+        TODO: Auto-stop the game when GameEnder works?
+        """
+        self.game = None
 
     @command("phase", admin=True, game=True)
-    def change_phase(self, source: str, *args, **kwargs):
-        """Changes the phase (admin action).
-
-        TODO: Implement!
-        """
+    def change_phase(self, source: str, new_phase: Optional[str] = None):
+        """Changes the phase (admin action)."""
+        if self.game is None:
+            raise RuntimeError("Game not yet started.")
+        self.game.change_phase(new_phase=new_phase)
+        # Alternatively:
+        # e = ETryPhaseChange(self.game, new_phase=new_phase)
+        # self.game.process_event(e)
 
     @command("do", game=True)
-    def do(self, source: str, cmd: str, *args, **kwargs):
-        """Activates an ability.
-
-        TODO: Implement!
-        """
+    def do(self, source: str, abil_name: str, *args, **kwargs):
+        """Activates an ability."""
+        abil_path = get_path(source, ABILITY, abil_name)
+        e = EActivate(self.game, abil_path, *args, **kwargs)
+        self.game.process_event(e)
 
     @command("vote", game=True)
     def vote(self, source: str, *args, **kwargs):
@@ -242,3 +300,5 @@ class CommandRunner(Generic[TUser]):
         # TODO: 'vote' for "unvote-all" or smth?
         # TODO: Maybe add this only in ForumCommandRunner?
         return self.do(source, "vote", *args, **kwargs)
+
+    # TODO: Maybe allow backup command handlers for unknown commands? :)
