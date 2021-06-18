f9ff3931b1cfdd4f6b9c935f65b9d40e387de420
Anatoly Makarevich <git@nowan.dev>
2021-06-18 20:40:13 +0200

    Added more import info, for good measure.

--- open_mafia_engine/commands/all.py
+++ open_mafia_engine/commands/all.py
@@ -8,7 +8,11 @@ from .parser import AbstractCommandParser, ShellCommandParser
 from .raw import RawCommand, TUser
 from .runner import CommandHandler, CommandRunner, command
 
+PYGMENTS_AVAILABLE: bool
 try:
     from .pygment_lexer import MafiaLexer
+
+    PYGMENTS_AVAILABLE = True
 except ImportError:
     _w.warn("Pygmants not installed, highlighting is not available.", ImportWarning)
+    PYGMENTS_AVAILABLE = False
