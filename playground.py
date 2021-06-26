23182024e11efbddf6f018b140118fc10fb61113
Anatoly Makarevich <git@nowan.dev>
2021-06-26 02:24:53 +0200

    Slightly cleaned up "playground"

--- playground.py
+++ playground.py
@@ -1,15 +1,11 @@
 from typing import List, Optional, Union
 
-from open_mafia_engine.api import *
+import open_mafia_engine.api as mafia
 
 # Testing game state
 
-builder = GameBuilder.load("test")
+builder = mafia.GameBuilder.load("test")
 game = builder.build(["Alice", "Bob", "Charlie"])
-tally: Tally = game.aux.filter_by_type(Tally)[0]
-
-
-# Do fake stuff
 
 alice, bob, charlie = game.actors
 # Alice: Vote, Mafia Kill
@@ -17,17 +13,17 @@ alice, bob, charlie = game.actors
 # Charlie: Vote, Protect
 
 game.change_phase()  # start the day
-game.process_event(EActivate(game, "Alice/ability/Vote", target="Bob"))
-game.process_event(EActivate(game, "Bob/ability/Vote", target="Alice"))
+game.process_event(mafia.EActivate(game, "Alice/ability/Vote", target="Bob"))
+game.process_event(mafia.EActivate(game, "Bob/ability/Vote", target="Alice"))
 
 
 game.change_phase()  # start the night
 assert not any(x.status["dead"] for x in game.actors)
 
 # Mafia Alice tries to kill Bob
-game.process_event(EActivate(game, "Alice/ability/Mafia Kill", target=bob))
+game.process_event(mafia.EActivate(game, "Alice/ability/Mafia Kill", target=bob))
 # ... but Charlie protected bob, with higher priority
-game.process_event(EActivate(game, "Charlie/ability/Protect", target=bob))
+game.process_event(mafia.EActivate(game, "Charlie/ability/Protect", target=bob))
 
 game.change_phase()  # process the night phase - nobody will die!
 
